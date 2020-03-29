9fd39df471e119b7f78cd48108c22176064fe2eb
BILLET MANON p1805662 <manon.billet@etu.univ-lyon1.fr>
2020-03-29 19:22:03 +0200

    modif test perso

--- src/Personnages.cpp
+++ src/Personnages.cpp
@@ -183,22 +183,28 @@ using namespace std;
 	}
 
 
-	void Hero::Bas() {
+	int Hero::Bas() {
 		if (positionValide(x, (y-1)))
 			y--;
+
+		return 0;
 	}
 
-	void Hero::Gauche() {
+	int Hero::Gauche() {
 		if (positionValide((x-1), y))
 			x--;
+
+		return 0;
 	}
 
-	void Hero::Droite() {
+	int Hero::Droite() {
 		if (positionValide((x+1), y))
 			x++;
+
+		return 0;
 	}
 
-	void Hero::deplacement() {
+	int Hero::deplacement() {
 		char touche;
 		cin>>touche;
 		switch (touche) {
@@ -215,16 +221,20 @@ using namespace std;
 						Droite();
 						break;
 		}
+		return 0;
 	}
 
-	void Hero::combat(Personnage* e) {
+	int Hero::combat(Personnage* e) {
 		if((e->getX() == x-1) || (e->getX() == x+1)
 		|| (e->getY() == y-1) || (e->getY() == y+1))
 			e->degat(atk);
+
+		return 0;
 	}
 
-	void Hero::setName(string sname) {
+	int Hero::setName(string sname) {
 			name = sname;
+			return 0;
 	}
 
 
@@ -259,7 +269,7 @@ using namespace std;
 
 	Ennemi::~Ennemi() {}
 
-	void Ennemi::deplacement(const Hero* h) {
+	int Ennemi::deplacement(const Hero* h) {
 		if(h->getX() < x && positionValide((x+1), y))
 			x++;
 		if(h->getX() > x && positionValide((x-1), y))
@@ -268,27 +278,27 @@ using namespace std;
 			y++;
 		if(h->getY() > y && positionValide(x, (y-1)))
 			y--;
+
+		return 0;
 	}
 
-	void Ennemi::combat(Hero* h) {
+	int Ennemi::combat(Hero* h) {
 		if((h->getX() == x-1) || (h->getX() == x+1)
 		|| (h->getY() == y-1) || (h->getY() == y+1))
 			h->degat(atk);
+
+		return 0;
 	}
 
 	int main(){
 
 		Hero h;
-		int adp = atk+def+pv;
-		h.lvUp();
-		assert(adp == atk+def+pv+3);
-		Bas();
-		assert(y == y--);
-		Haut();
-		assert(y == y++);
-		Gauche();
+		assert(h.lvUp() == 0);
+		assert(h.Bas() == 0);
+		assert(h.Gauche() == 0);
+
 		assert(x == x--);
-		Droite();
+
 		assert(x == x++);
 		h.deplacement();
 		assert();
