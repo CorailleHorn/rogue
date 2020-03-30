fa96d45e7b16e40d14971d870607e031890a59c4
SAURY JEAN p1805563 <jean.saury@etu.univ-lyon1.fr>
2020-03-30 15:43:22 +0200

    gen_bug_report

--- src/Generation.cpp
+++ src/Generation.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char *argv[], char **envp)
 {
 	//on peut écrire la seed manuellement ...
-	//int seed = 1585401860;
+	//int const seed = 1585500534;
 	// ... ou la générer avec time(NULL)
 	int const seed = time(NULL);
 	srand(seed);
